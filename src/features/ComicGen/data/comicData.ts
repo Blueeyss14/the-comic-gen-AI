ed740b59a031511cc986aa19bb845a4dea2881e6
unknown <delkanoberutu8@gmail.com>
2026-03-07 14:15:00 +0700

    add some art styles

--- src/features/ComicGen/data/comicData.ts
+++ src/features/ComicGen/data/comicData.ts
@@ -1,5 +1,5 @@
 export const genre = ['Romance', 'Comedy', 'Action', 'Slice of Life', 'Sci-Fi', 'Horror','Adventure', 'Drama', 'Sports', 'Historical'];
 export const mode = ['Color', 'Monochrome'];
-export const artStyle = ['Manga', 'Manwa', 'Cartoon'];
+export const artStyle = ['Manga', 'Manhwa', 'Cartoon', 'Pixel Art'];
 export const panels = ['2 Panels', '4 Panels', '6 Panels', '8 Panels'];
 export const ratio = ['16:9', '9:16', '1:1'];
